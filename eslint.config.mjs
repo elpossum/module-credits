a5dde3d16de952dcbf836e3d8e773220a2f69daf
elpossum <136785378+elpossum@users.noreply.github.com>
2025-08-16 06:39:59 +0100

    Make linting more specific

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-export default [
+export default defineConfig([
   { languageOptions: { globals: globals.browser } },
+  globalIgnores(["**/libraries/", "dist/"]),
   pluginJs.configs.recommended,
   eslintConfigPrettier,
-];
+]);
