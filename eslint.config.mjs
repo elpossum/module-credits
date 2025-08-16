abc52674ca0ff6ba8c1b0d3a9f2365859583ab37
elpossum <136785378+elpossum@users.noreply.github.com>
2025-08-16 23:34:40 +0100

    Define globals for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,34 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        game: "readonly",
+        $: "readonly",
+        ui: "readonly",
+        FormApplication: "readonly",
+        saveDataToFile: "readonly",
+        SocketInterface: "readonly",
+        ModuleManagement: "readonly",
+        SettingsConfig: "readonly",
+        Dialog: "readonly",
+        foundry: "readonly",
+        mergeObject: "readonly",
+        showdown: "readonly",
+        Hooks: "readonly",
+        Handlebars: "readonly",
+        renderTemplate: "readonly",
+        libWrapper: "readonly",
+        socketlib: "readonly",
+        FilePicker: "readonly",
+        isNewerVersion: "readonly",
+        readTextFromFile: "readonly",
+        ContextMenu: "readonly",
+      }
+    },
+  },
   globalIgnores(["**/libraries/", "dist/"]),
   pluginJs.configs.recommended,
   eslintConfigPrettier,
